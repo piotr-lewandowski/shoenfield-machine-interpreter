3e56ea1eab34ef55f82eae52c5bc18b0fc1ccb5d
bartekkdz <bartekdziwirek@gmail.com>
2023-03-29 20:05:56 +0200

    add Reader.hs

---
+++ src/Reader.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE BangPatterns #-}
+{-# OPTIONS -Wall #-}
+module Reader where
+
+import System.IO
+import System.FilePath ()
+import Control.Exception (try)
+import System.Directory.Internal.Prelude   
+
+reader :: IO String
+reader =  do
+    putStr "Enter file path: "
+    hFlush stdout
+    filePath <- getLine
+    print ("Opening file " ++ filePath)
+    value <- tryIOError (readFile filePath)
+    case value of
+        Left err -> do
+            print ("Error: Couldn't open file " ++ filePath ++ ". Try again.")
+            reader
+        Right content -> do
+            let cleanContent = map (\c -> if c == '\n' then ' ' else c) content -- remove newline characters
+            print ("Successfully opened file " ++ filePath)
+            return cleanContent
