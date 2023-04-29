8c3a89ca737600a4efdd448f373f6e9f31fe18d3
Piotr Lewandowski <lewandowskipiotr98@gmail.com>
2023-04-29 22:31:23 +0200

    Added argument parsing

--- src/Reader.hs
+++ src/Reader.hs
@@ -1,23 +1,26 @@
-{-# LANGUAGE BangPatterns #-}
-{-# OPTIONS -Wall #-}
-module Reader where
+module Reader (reader) where
 
-import System.IO
+import System.Directory.Internal.Prelude
 import System.FilePath ()
-import Control.Exception (try)
-import System.Directory.Internal.Prelude   
+import System.IO
 
-reader :: IO String
-reader =  do
-    putStr "Enter file path: "
-    hFlush stdout
-    filePath <- getLine
-    print ("Opening file " ++ filePath)
-    value <- tryIOError (readFile filePath)
-    case value of
-        Left err -> do
-            print ("Error: Couldn't open file " ++ filePath ++ ". Try again.")
-            reader
-        Right content -> do
-            print ("Successfully opened file " ++ filePath)
-            return filePath
+reader :: IO (String, String)
+reader = do
+  program <- readPath "program"
+  args <- readPath "arguments"
+  pure (program, args)
+
+readPath :: String -> IO String
+readPath name = do
+  putStr $ "Enter " ++ show name ++ " file path:"
+  hFlush stdout
+  filePath <- getLine
+  print ("Opening file " ++ filePath)
+  value <- tryIOError (readFile filePath)
+  case value of
+    Left _ -> do
+      print ("Error: Couldn't open file " ++ filePath ++ ". Try again.")
+      readPath name
+    Right _ -> do
+      print ("Successfully opened file " ++ filePath)
+      return filePath
